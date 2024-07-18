ad8e634d1a193b0a0c6777c17ab5b3c19b418837
Alexander Muszynski <amuszynski@whisker.com>
2024-07-18 17:10:15 -0400

    oil updates, telescope config mapping

--- init.lua
+++ init.lua
@@ -290,6 +290,7 @@ require('lazy').setup({
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+        ['<leader>g'] = { name = '[G]it Search', _ = 'which_key_ignore' },
       }
       -- visual mode
       require('which-key').register({
@@ -380,10 +381,15 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>gs', '<CMD>:Telescope git_status initial_mode=normal<CR>', { desc = '[G]it [S]tatus' })
+      vim.keymap.set('n', '<leader>gt', '<CMD>:Telescope git_stash initial_mode=normal<CR>', { desc = '[G]it s[T]ash' })
+      vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = '[G]it [C]ommits' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>st', builtin.treesitter, { desc = '[S]earch [T]reesitter' })
+      vim.keymap.set('n', '<leader>th', builtin.colorscheme, { desc = '[TH]eme Picker' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader><leader>', '<CMD>:Telescope buffers initial_mode=normal<CR>', { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -609,7 +615,6 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         -- Lua
-        'lua-language-server',
         'stylua',
         -- TS / JS
         'html-lsp',
@@ -618,7 +623,6 @@ require('lazy').setup({
         'prettierd',
         'typescript-language-server',
         -- Python
-        'ruff-lsp',
         'ruff',
         'pyright',
         -- C
@@ -800,6 +804,17 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
+    opts = { transparent_background = true },
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+    priority = 1000,
   },
 
   -- Highlight todo, notes, etc in comments
@@ -858,6 +873,9 @@ require('lazy').setup({
         'vimdoc',
         'css',
         'python',
+        'go',
+        'rust',
+        'javascript',
         'typescript',
         'graphql',
       },
