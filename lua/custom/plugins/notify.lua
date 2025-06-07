ee11ca87bb47c3ee29938db9ab669a219a9fa601
Alex Muszynski <alexander.muszynski@gmail.com>
2025-06-07 07:55:19 -0400

    remove notify, was annoying, maybe need to config more

--- lua/custom/plugins/notify.lua
+++ lua/custom/plugins/notify.lua
@@ -1,7 +1,7 @@
 return {
-  'rcarriga/nvim-notify',
-  event = 'VeryLazy',
-  opts = {
-    background_colour = '#000000',
-  },
+  -- 'rcarriga/nvim-notify',
+  -- event = 'VeryLazy',
+  -- opts = {
+  --   background_colour = '#000000',
+  -- },
 }
