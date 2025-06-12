1aedadccd42f109870117d7e826a5b629189444c
Alex Muszynski <alexander.muszynski@gmail.com>
2025-06-11 20:09:59 -0400

    change theme, add md preview back, disable notify

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -3,16 +3,14 @@ return { -- You can easily change to a different colorscheme.
   -- change the command in the config to whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'mofiqul/vscode.nvim',
+  'folke/tokyonight.nvim',
+  lazy = false,
   opts = { transparent = true, styles = { sidebars = 'transparent', floats = 'transparent' } },
   init = function()
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'vscode'
-
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
+    vim.cmd.colorscheme 'tokyonight-night'
   end,
   priority = 1000,
 }
